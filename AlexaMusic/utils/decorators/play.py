5e1d33675fc3eb643a902cd1495f0b38300fa434
mahmoud7641 <152595992+mahmoud7641@users.noreply.github.com>
2023-12-20 09:33:29 +0200

    Update play.py

--- AlexaMusic/utils/decorators/play.py
+++ AlexaMusic/utils/decorators/play.py
@@ -1,6 +1,6 @@
 
 # Copyright (C) 2021-2022 by Alexa_Help@Github, < https://github.com/Jankarikiduniya >.
-# A Powerful Music Bot Property Of Rocks Indian Largest Chatting Group
+# A Powerful Music Bot roperty Of Rocks Indian Largest Chatting Group
 
 # Kanged By © @Dr_Asad_Ali
 # Rocks © @Shayri_Music_Lovers
